bcfca443689569ff62986bce236a9cb36bdc1a36
vishesh99yadav <vishesh99yadav@gmail.com>
2025-11-30 01:49:35 -0800

    Add header file for manager history view function

---
+++ managerhistoryview.h
@@ -0,0 +1,9 @@
+#ifndef MANAGERHISTORYVIEW_H
+#define MANAGERHISTORYVIEW_H
+
+#include "inventory.h"
+
+// Function to display history of fulfilled orders
+void manager_history_view();
+
+#endif // MANAGERHISTORYVIEW_
