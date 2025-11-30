73cdb3fea1622ed8b620ded7ff629be315d0386e
Mayank Awasthi <business.mayankawasthi06@gmail.com>
2025-11-30 10:39:57 +0530

    Add inventory header file with item structure
    Commit--011

---
+++ inventory.h
@@ -0,0 +1,19 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#define MAX_ITEMS 100
+
+typedef struct {
+    int id;
+    char name[50];
+    int qty;
+} Item;
+
+extern Item inventory_list[MAX_ITEMS];
+extern int inventory_count;
+
+void add_stock();
+void show_inventory();
+int stock_left(int id);
+
+#endif
